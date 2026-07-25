a4d9340d6d2ed2b70c9bd25290aa61b3df4eb007
aiwithwaw-del <a.i.withwaw@gmail.com>
2026-07-26 01:56:41 +0300

    Create lib/features/auth/domain/repositories/auth_repository.dart

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failures.dart';
+import '../entities/user_entity.dart';
+
+abstract class AuthRepository {
+  Stream<UserEntity?> get authStateChanges;
+  
+  Future<Either<Failure, UserEntity>> loginWithEmail({
+    required String email,
+    required String password,
+  });
+  
+  Future<Either<Failure, UserEntity>> registerWithEmail({
+    required String email,
+    required String password,
+    required String displayName,
+  });
+  
+  Future<Either<Failure, UserEntity>> signInWithGoogle();
+  
+  Future<Either<Failure, UserEntity>> signInWithApple();
+  
+  Future<Either<Failure, void>> sendEmailVerification();
+  
+  Future<Either<Failure, void>> sendPasswordResetEmail(String email);
+  
+  Future<Either<Failure, void>> verifyPhoneNumber(String phoneNumber);
+  
+  Future<Either<Failure, void>> updateProfile({
+    String? displayName,
+    String? photoUrl,
+  });
+  
+  Future<void> signOut();
+}
