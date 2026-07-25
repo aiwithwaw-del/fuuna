af76e7d1acb659781a9c8a6c1d562b0dce46044b
aiwithwaw-del <a.i.withwaw@gmail.com>
2026-07-26 01:47:22 +0300

    Create lib/config/routes/route_names.dart

---
+++ lib/config/routes/route_names.dart
@@ -0,0 +1,21 @@
+class RouteNames {
+  RouteNames._();
+  
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String emailVerification = '/email-verification';
+  static const String forgotPassword = '/forgot-password';
+  static const String home = '/home';
+  static const String challengeDetails = '/challenge/:id';
+  static const String search = '/search';
+  static const String categories = '/categories';
+  static const String myChallenges = '/my-challenges';
+  static const String wallet = '/wallet';
+  static const String profile = '/profile';
+  static const String settings = '/settings';
+  static const String sponsorDashboard = '/sponsor';
+  static const String createChallenge = '/sponsor/create';
+  static const String adminDashboard = '/admin';
+}
