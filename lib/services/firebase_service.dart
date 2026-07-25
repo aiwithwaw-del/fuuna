301e7b30fa389d65c33cf740fd018c9c2adc578a
aiwithwaw-del <a.i.withwaw@gmail.com>
2026-07-26 01:44:55 +0300

    Create lib/services/firebase_service.dart

---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class FirebaseService {
+  static final FirebaseAuth auth = FirebaseAuth.instance;
+  static final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  static final FirebaseStorage storage = FirebaseStorage.instance;
+  static final FirebaseMessaging messaging = FirebaseMessaging.instance;
+  
+  static Future<void> initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    
+    // Crashlytics
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+    
+    // Messaging
+    await messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+    
+    // Firestore offline persistence
+    await firestore.enablePersistence();
+  }
+}
