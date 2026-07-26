d27a38d56c649aade56e41d8460a5e737c48bf68
aiwithwaw-del <a.i.withwaw@gmail.com>
2026-07-26 09:54:36 +0300

    Update failures.dart

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -3,9 +3,9 @@ import 'package:equatable/equatable.dart';
 abstract class Failure extends Equatable {
   final String message;
   final String? code;
-  
+
   const Failure({required this.message, this.code});
-  
+
   @override
   List<Object?> get props => [message, code];
 }
