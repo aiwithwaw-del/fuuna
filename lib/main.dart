0bb3a89814a655037ccb4c77198fc8e62e288f2a
aiwithwaw-del <a.i.withwaw@gmail.com>
2026-07-26 09:53:12 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'services/firebase_service.dart';
-import 'app.dart';
+import 'package:fuuna/services/firebase_service.dart';
+import 'package:fuuna/app.dart';
 
 void main() async {
   await FirebaseService.initialize();
-  
+
   runApp(
     const ProviderScope(
       child: FuunaApp(),
